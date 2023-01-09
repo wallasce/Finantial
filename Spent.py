0bd1813e53416d5f505ad07785aad8204d5a7cfa
wallasce <lwallasce@outlook.com>
2023-01-09 11:25:47 -0300

    Add constructor to Spent

--- Spent.py
+++ Spent.py
@@ -1,3 +1,10 @@
+import datetime
+
 class Spent:
-    def __init__(self) -> None:
+    def __init__(self, name: str, value: float, date: datetime, type: str, form: str) -> None:
+        self.name = name
+        self.value = value
+        self.date = date
+        self.type = type
+        self.form = form
         pass
