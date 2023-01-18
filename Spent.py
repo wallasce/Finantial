a643e204a00fda3add9c3924784ceede890c8d1e
wallasce <lwallasce@outlook.com>
2023-01-18 10:51:50 -0300

    Rename parameters from Spent

--- Spent.py
+++ Spent.py
@@ -1,8 +1,8 @@
 import datetime
 
 class Spent:
-    def __init__(self, name: str, value: float, date: datetime, type: str, form: str) -> None:
-        self.name = name
+    def __init__(self, description: str, value: float, date: datetime, type: str, form: str) -> None:
+        self.description = description
         self.value = value
         self.date = date
         self.type = type
