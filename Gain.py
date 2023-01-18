6af98d84f869595f723744af7ea39805fd07053b
wallasce <lwallasce@outlook.com>
2023-01-18 10:50:10 -0300

    Create Class Gain

---
+++ Gain.py
@@ -0,0 +1,10 @@
+import datetime
+
+class Gain:
+    def __init__(self, description: str, value: float, date: datetime) -> None:
+        self.description = description
+        self.value = value
+        self.date = date
+        self.type = type
+        self.form = form
+        pass
