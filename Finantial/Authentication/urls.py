37bb2978dd721fd8003d50b3fdd4c0a28d7f9834
wallasce <lwallasce@outlook.com>
2024-03-13 14:10:59 -0300

    Feat: Create Register User

--- Finantial/Authentication/urls.py
+++ Finantial/Authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from Authentication.views import ObtainTokenPairView
+from Authentication.views import ObtainTokenPairView, RegisterView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
     path('login/', ObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
 ]
